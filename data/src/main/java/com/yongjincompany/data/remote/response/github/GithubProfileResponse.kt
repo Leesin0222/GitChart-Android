c408e96cfbfaebe2ce768d0af6e43fc81c24f0f6
Leesin0222 <dragon02229@naver.com>
2022-08-22 14:06:17 +0900

    ⚡ :: GithubProfileResponse 작성

---
+++ data/src/main/java/com/yongjincompany/data/remote/response/github/GithubProfileResponse.kt
@@ -0,0 +1,21 @@
+package com.yongjincompany.data.remote.response.github
+
+import com.google.gson.annotations.SerializedName
+import com.yongjincompany.domain.entity.GithubProfileEntity
+
+data class GithubProfileResponse(
+    @SerializedName("avatar_url") val avatarUrl: String,
+    @SerializedName("followers") val followers: Int,
+    @SerializedName("following") val following: Int,
+    @SerializedName("login") val login: String,
+    @SerializedName("name") val name: String,
+)
+
+fun GithubProfileResponse.toEntity() =
+    GithubProfileEntity(
+        avatarUrl = avatarUrl,
+        followers = followers,
+        following = following,
+        login = login,
+        name = name
+    )
