d8cff9bdb8bac1d6c6e1e1ad19b5940c06a86462
Leesin0222 <dragon02229@naver.com>
2022-08-22 14:10:37 +0900

    ⚡ :: RemoteDataSourceImpl에 깃허브 프로필 받아오는 dataSource구현체 작성

---
+++ data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteGithubDataSourceImpl.kt
@@ -0,0 +1,18 @@
+package com.yongjincompany.data.remote.datasource
+
+import com.yongjincompany.data.remote.api.github.GitHubApi
+import com.yongjincompany.data.remote.response.github.GithubProfileResponse
+import com.yongjincompany.data.remote.response.github.toEntity
+import com.yongjincompany.data.util.HttpHandler
+import com.yongjincompany.domain.entity.GithubProfileEntity
+import javax.inject.Inject
+
+class RemoteGithubDataSourceImpl @Inject constructor(
+    private val gitHubApi: GitHubApi
+) : RemoteGithubDataSource {
+    override suspend fun fetchGithubProfile(userName: String): GithubProfileEntity =
+        HttpHandler<GithubProfileResponse>()
+            .httpRequest { gitHubApi.getGithubProfile(userName) }
+            .sendRequest().toEntity()
+
+}
