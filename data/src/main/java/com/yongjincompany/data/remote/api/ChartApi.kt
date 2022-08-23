f8c23e7a2f0c3174d0577b841df058214f184420
Leesin0222 <dragon02229@naver.com>
2022-08-23 20:01:46 +0900

    ⚡ :: 주간 커밋 수 받아오는 api 작성

--- data/src/main/java/com/yongjincompany/data/remote/api/ChartApi.kt
+++ data/src/main/java/com/yongjincompany/data/remote/api/ChartApi.kt
@@ -1,6 +1,7 @@
 package com.yongjincompany.data.remote.api
 
 import com.yongjincompany.data.remote.response.chart.DayCommitResponse
+import com.yongjincompany.data.remote.response.chart.WeeklyCommitResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -9,4 +10,9 @@ interface ChartApi {
     suspend fun getDayCommit(
         @Path("username") userName: String
     ): DayCommitResponse
+
+    @GET("{username}/weekcount")
+    suspend fun getWeeklyCommit(
+        @Path("username") userName: String
+    ): WeeklyCommitResponse
 }
