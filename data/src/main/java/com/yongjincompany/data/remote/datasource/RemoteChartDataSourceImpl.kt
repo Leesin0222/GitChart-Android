25bccc9a70809833083b421194e6f1a1b102ec5e
Leesin0222 <dragon02229@naver.com>
2022-08-23 20:01:25 +0900

    ⚡ :: RemoteDataSourceImpl에 주간 커밋  수 받아오는 dataSource 구현체 작성

--- data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteChartDataSourceImpl.kt
+++ data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteChartDataSourceImpl.kt
@@ -2,9 +2,11 @@ package com.yongjincompany.data.remote.datasource
 
 import com.yongjincompany.data.remote.api.ChartApi
 import com.yongjincompany.data.remote.response.chart.DayCommitResponse
+import com.yongjincompany.data.remote.response.chart.WeeklyCommitResponse
 import com.yongjincompany.data.remote.response.chart.toEntity
 import com.yongjincompany.data.util.HttpHandler
 import com.yongjincompany.domain.entity.chart.DayCommitEntity
+import com.yongjincompany.domain.entity.chart.WeeklyCommitEntity
 import javax.inject.Inject
 
 class RemoteChartDataSourceImpl @Inject constructor(
@@ -13,6 +15,11 @@ class RemoteChartDataSourceImpl @Inject constructor(
     override suspend fun fetchDayCommit(userName: String): DayCommitEntity =
         HttpHandler<DayCommitResponse>()
             .httpRequest { chartApi.getDayCommit(userName) }
+            .sendRequest().toEntity()
+
+    override suspend fun fetchWeeklyCommit(userName: String): WeeklyCommitEntity =
+        HttpHandler<WeeklyCommitResponse>()
+            .httpRequest { chartApi.getWeeklyCommit(userName) }
             .sendRequest().toEntity()
 
 }
