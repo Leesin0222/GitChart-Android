9b7b4d4b36b294c5a3f5bf29d17cc21c6bd20ef8
Leesin0222 <dragon02229@naver.com>
2022-08-23 20:26:23 +0900

    ⚡ :: RemoteDataSource에 연간 커밋 수 받아오는 interface 함수 작성

--- data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteChartDataSource.kt
+++ data/src/main/java/com/yongjincompany/data/remote/datasource/RemoteChartDataSource.kt
@@ -2,9 +2,12 @@ package com.yongjincompany.data.remote.datasource
 
 import com.yongjincompany.domain.entity.chart.DayCommitEntity
 import com.yongjincompany.domain.entity.chart.WeeklyCommitEntity
+import com.yongjincompany.domain.entity.chart.YearCommitEntity
 
 interface RemoteChartDataSource {
     suspend fun fetchDayCommit(userName: String): DayCommitEntity
 
     suspend fun fetchWeeklyCommit(userName: String): WeeklyCommitEntity
+
+    suspend fun fetchYearCommit(userName: String): YearCommitEntity
 }
