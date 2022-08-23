387a601484c960830786b67b19cffd408f52c021
Leesin0222 <dragon02229@naver.com>
2022-08-23 20:27:34 +0900

    ⚡ :: Repository에 연간 커밋 수 받아오는 interface 함수 작성

--- domain/src/main/java/com/yongjincompany/domain/repository/ChartRepository.kt
+++ domain/src/main/java/com/yongjincompany/domain/repository/ChartRepository.kt
@@ -2,10 +2,13 @@ package com.yongjincompany.domain.repository
 
 import com.yongjincompany.domain.entity.chart.DayCommitEntity
 import com.yongjincompany.domain.entity.chart.WeeklyCommitEntity
+import com.yongjincompany.domain.entity.chart.YearCommitEntity
 import kotlinx.coroutines.flow.Flow
 
 interface ChartRepository {
     suspend fun fetchDayCommit(userName: String): Flow<DayCommitEntity>
 
     suspend fun fetchWeeklyCommit(userName: String): Flow<WeeklyCommitEntity>
+
+    suspend fun fetchYearCommit(userName: String): Flow<YearCommitEntity>
 }
