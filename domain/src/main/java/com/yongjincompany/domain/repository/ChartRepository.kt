cb5ea13b0fba7e0f8462beb861c59ca3f5e97bd6
Leesin0222 <dragon02229@naver.com>
2022-08-23 20:02:29 +0900

    ⚡ :: repository에 주간 커밋 수 받아오는 interface 함수 작성

--- domain/src/main/java/com/yongjincompany/domain/repository/ChartRepository.kt
+++ domain/src/main/java/com/yongjincompany/domain/repository/ChartRepository.kt
@@ -1,8 +1,11 @@
 package com.yongjincompany.domain.repository
 
 import com.yongjincompany.domain.entity.chart.DayCommitEntity
+import com.yongjincompany.domain.entity.chart.WeeklyCommitEntity
 import kotlinx.coroutines.flow.Flow
 
 interface ChartRepository {
     suspend fun fetchDayCommit(userName: String): Flow<DayCommitEntity>
+
+    suspend fun fetchWeeklyCommit(userName: String): Flow<WeeklyCommitEntity>
 }
